3758c7ce314813e9cc4a17ce2c5d8ed55ecfffa0
PedroH-Neves <PedroHDN710@gmail.com>
2023-10-06 21:18:18 -0400

    Correct menu options

--- lib/helpers/options.rb
+++ lib/helpers/options.rb
@@ -12,7 +12,8 @@ module Options
     puts '9 - Exit'
   end
 
-  def menu_options = {
+  def menu_options =
+    {
       1 => 'list_of_books',
       2 => 'list_of_music',
       3 => 'list_of_genres',
@@ -22,5 +23,4 @@ module Options
       7 => 'add_book',
       8 => 'add_music'
     }
-  end
 end
